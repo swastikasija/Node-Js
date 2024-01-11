51cb5aaafa2fb5c6859e0b3d35f45b33657a4674
swastik <swastikasija072@gmail.com>
2024-01-11 22:20:15 +0530

    Send File Done

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const server = express();
 
 server.get("/", (req, res) => {
-  res.send("Hello Express")
+  res.send("<h1>Hello Express</h1>");
+  res.sendFile('D:\Node Js\index.js')
 });
 
 server.listen(8080, function () {
