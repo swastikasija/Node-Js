7c73f6d8c6d68b8c716abf262396f70bec1b61e5
swastik <swastikasija072@gmail.com>
2024-01-23 21:14:10 +0530

    index.js updated

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { data } = require("./data.js");
 const app = express();
 
 app.get("/", (req, res) => {
@@ -6,7 +7,15 @@ res.status(200).json({
   message: "App running",
 })
 });
-
+app.get('/data/get', function (req, res) {
+  try {
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(500).json({
+      error: "Server Error",
+    })
+  }
+})
 app.listen(4000, () => {
   console.log("server started")
 });
