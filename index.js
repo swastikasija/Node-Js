a26f9c7ceaea98ccdf5115684c8365bfebb8580d
swastik <swastikasija072@gmail.com>
2024-01-23 21:03:40 +0530

    index.js updated

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-
+res.status(200).json({
+  message: "App running",
+})
 });
 
-server.listen(8080, function () {
-  console.log("Systumm Hanged");
+app.listen(4000, () => {
+  console.log("server started")
 });
+
